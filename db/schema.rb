bcc53e66531b34c9c98629ba6f1ca231f4f16cad
Kylie <kylie.rae.slattery@gmail.com>
2019-06-07 22:58:58 +0000

    added favorites model and database

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_01_012938) do
+ActiveRecord::Schema.define(version: 2019_06_07_225626) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "favorites", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "places", force: :cascade do |t|
     t.text "description"
